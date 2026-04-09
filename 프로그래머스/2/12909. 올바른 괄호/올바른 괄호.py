36de8eac65d587c136172473ed83bf49087e32d0
Dchat <ksmjbh0745@gmail.com>
2026-04-09 21:49:51 +0900

    [level 2] Title: 올바른 괄호, Time: 6.95 ms, Memory: 9.52 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,15 @@
+def solution(s):
+    temp = []
+    for i in s:
+        if i == '(':
+            temp.append(i)
+        else:
+            if temp:
+                temp.pop()
+            else:
+                return False
+    
+    if temp:
+        return False
+    else: 
+        return True
