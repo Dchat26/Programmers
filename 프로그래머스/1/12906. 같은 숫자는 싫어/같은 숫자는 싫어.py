659aa593b22da93636760f9e0007c5db306c2733
Dchat <ksmjbh0745@gmail.com>
2026-04-09 21:14:48 +0900

    Update 같은 숫자는 싫어.py

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,12 +1,7 @@
 def solution(arr):
     answer = []
-    current = -1
     for i in arr:
-        if current == -1:
-            current = i
-        elif current != i:
-            answer.append(current)
-            current = i
-    answer.append(i)
+        if not answer or answer[-1] != i:
+            answer.append(i)
             
     return answer
