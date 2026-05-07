4904308b60cea3574d45b0a0f4634ded30801506
Dchat <ksmjbh0745@gmail.com>
2026-05-07 23:52:02 +0900

    전체 코드 업로드 -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n) {
+    vector<int> prime(n + 1, 1);
+    prime[0] = 0;
+    prime[1] = 0;
+    
+    for (int i = 2; i * i <= n; i++) {
+        if (prime[i] == 1) {
+            for (int j = i * i; j <= n; j += i) {
+                prime[j] = 0;
+            }
+        }
+    }
+    
+    return count(prime.begin(), prime.end(), 1);
+}
