999e53c1df9e2e2b9d60a32d35e2a9475afbe145
CHAT CAN <jungminwoo2454@gmail.com>
2026-05-12 13:14:32 +0900

    del

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++
@@ -1,8 +0,0 @@
-def solution(data, ext, val_ext, sort_by):
-    col = {"code": 0, "date": 1, "maximum": 2, "remain": 3}
-    
-    filtered_data = [item for item in data if item[col[ext]] < val_ext]
-    
-    filtered_data.sort(key=lambda x: x[col[sort_by]])
-    
-    return filtered_data
