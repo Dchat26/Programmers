33ee3b99dc27bf0cf194a07fa2a69a37d6b27480
Dchat <ksmjbh0745@gmail.com>
2026-06-05 23:36:54 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.20 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -0,0 +1,40 @@
+def solution(numbers, hand):
+    answer = ''
+    keypad = {
+        1: (0, 0), 2: (0, 1), 3: (0, 2),
+        4: (1, 0), 5: (1, 1), 6: (1, 2),
+        7: (2, 0), 8: (2, 1), 9: (2, 2),
+        '*': (3, 0), 0: (3, 1), '#': (3, 2)
+    }
+    
+    left_pos = keypad['*']
+    right_pos = keypad['#']
+    
+    for num in numbers:
+        if num in (1, 4, 7):
+            answer += 'L'
+            left_pos = keypad[num]
+        elif num in (3, 6, 9):
+            answer += 'R'
+            right_pos = keypad[num]
+        else:
+            target_pos = keypad[num]
+            
+            left_dist = abs(left_pos[0] - target_pos[0]) + abs(left_pos[1] - target_pos[1])
+            right_dist = abs(right_pos[0] - target_pos[0]) + abs(right_pos[1] - target_pos[1])
+            
+            if left_dist < right_dist:
+                answer += 'L'
+                left_pos = target_pos
+            elif right_dist < left_dist:
+                answer += 'R'
+                right_pos = target_pos
+            else:
+                if hand == 'left':
+                    answer += 'L'
+                    left_pos = target_pos
+                else:
+                    answer += 'R'
+                    right_pos = target_pos
+                    
+    return answer
