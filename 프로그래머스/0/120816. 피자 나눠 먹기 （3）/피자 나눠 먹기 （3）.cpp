4948dd66764cdb21d770276f4ff248259f36a27f
Dchat <ksmjbh0745@gmail.com>
2026-06-11 23:08:17 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int slice, int n) {
+    if (n <= slice) {
+        return 1;
+    }
+    else if (n % slice == 0) {
+        return n / slice;
+    }
+    else {
+        return n / slice + 1;
+    }
+}
